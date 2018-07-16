38d4383d22c1d602c1f6f3a7e87e81f1fae25049
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2018-07-17 06:27:05 +0700

    update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->text('gravatar');
             $table->rememberToken();
             $table->timestamps();
         });
