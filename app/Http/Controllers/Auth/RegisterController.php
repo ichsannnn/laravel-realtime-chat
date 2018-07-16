0fb53bd958a2a4d73429bda7f33406716b515651
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2018-07-17 06:46:02 +0700

    update auth

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Model\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -63,10 +63,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $hash = md5(strtolower(trim($data['email'])));
+        $gravatar = "https://www.gravatar.com/avatar/$hash";
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'gravatar' => $gravatar
         ]);
     }
 }
